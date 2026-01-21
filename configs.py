8d798b14e8faaf80c78c46c30e8045388186582f
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-01-21 20:03:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "21419016"))
     API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7930803136:AAHTD2G4SERQsjcDmlBEc0EQZE0yC5LETxk")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7299707119:AAHyKZdC7nM85vnxbiqbgX1cn6M7XGYxkk0")
 
     # Admin / Owner IDs
     SUDO = list(map(int, getenv(
